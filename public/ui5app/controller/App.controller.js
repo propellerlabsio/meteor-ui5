b0e959f493ea0606c8b3f77df28791be6030e79a
Peter Roehlen <peter.roehlen@gmail.com>
2015-10-26 09:24:00 +1300

    MeteorModel working but not reactive yet.

--- public/ui5app/controller/App.controller.js
+++ public/ui5app/controller/App.controller.js
@@ -8,19 +8,26 @@ sap.ui.define(
         "use strict";
         return Controller.extend("x.controller.App", {
             onInit: function() {
+                // Build and set JSON model from meteor collection
                 // Get people from meteor collection.  Autopublish won't work
                 // here as the data isn't loaded yet (it takes some time in the background)
                 // so we need to get the data via a subscription
                 Meteor.subscribe("people", {
                     onReady: () => {
                         var oPeople = people.find().fetch();
-                        var oPeopleModel = new JSONModel(oPeople);
-                        this.getView().setModel(oPeopleModel, "peopleModel");
+                        var oPeopleJSONModel = new JSONModel(oPeople);
+                        this.getView().setModel(oPeopleJSONModel, "peopleJSONModel");
                     },
                     onError: () => {
                         console.log("Subscription error", arguments);
                     }
                 });
+
+                // Build and set Meteor model from meteor subscription and cursor
+                var sSubscription = "people";
+                var oCursor = people.find();
+                var oPeopleMeteorModel = new MeteorModel(sSubscription,oCursor);
+                this.getView().setModel(oPeopleMeteorModel, "peopleMeteorModel");
             }
         });
     }
