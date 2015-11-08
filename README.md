fa82c96d4f2f726dec8c2f3e7b93c059f72f4886
Peter Roehlen <peter.roehlen@gmail.com>
2015-11-08 19:15:45 +0700

    Cleanup before creating new branch

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # meteor-ui5
-This repo is a work in progress for getting Open UI5 working with Meteor.
-
+This package is a work in progress for using Open UI5 with Meteor.  Apart from
+including the OpenUI5 libraries, it contains a new UI5 MeteorModel for reactively
+hooking meteor collections and queries to UI5 controls.
+# TODO
+* Two-way binding (update meteor collection from UI5 control changes)
+* Performance-optimizations in Meteor cursor observeChanges - only update, add and remove changed documents.
+* Destroy cursor queryHandle on destroy of UI5 model
+* Create meteor package for extension
--- public/ui5app/controller/App.controller.js
+++ public/ui5app/controller/App.controller.js
@@ -1,33 +1,17 @@
 sap.ui.define(
     [
         "sap/ui/core/mvc/Controller",
-        "sap/m/MessageToast",
-        "sap/ui/model/json/JSONModel"
+        "sap/m/MessageToast"
     ],
     function(Controller, MessageToast, JSONModel) {
         "use strict";
         return Controller.extend("x.controller.App", {
             onInit: function() {
-                // Build and set JSON model from meteor collection
-                // Get people from meteor collection.  Autopublish won't work
-                // here as the data isn't loaded yet (it takes some time in the background)
-                // so we need to get the data via a subscription
-                Meteor.subscribe("people", {
-                    onReady: () => {
-                        var oPeople = people.find().fetch();
-                        var oPeopleJSONModel = new JSONModel(oPeople);
-                        this.getView().setModel(oPeopleJSONModel, "peopleJSONModel");
-                    },
-                    onError: () => {
-                        console.log("Subscription error", arguments);
-                    }
-                });
-
                 // Build and set Meteor model from meteor subscription and cursor
                 var sSubscription = "people";
                 var oCursor = people.find();
                 var oPeopleMeteorModel = new MeteorModel(sSubscription,oCursor);
-                this.getView().setModel(oPeopleMeteorModel, "peopleMeteorModel");
+                this.getView().setModel(oPeopleMeteorModel, "people");
             }
         });
     }
