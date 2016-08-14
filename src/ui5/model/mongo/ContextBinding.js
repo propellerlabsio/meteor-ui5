1d86957d204d064c900153d487e99e73183112a4
Peter Roehlen <peter.roehlen@gmail.com>
2016-08-14 09:49:15 +0700

    ContextBinding jsdoc finished

--- src/ui5/model/mongo/ContextBinding.js
+++ src/ui5/model/mongo/ContextBinding.js
@@ -6,12 +6,15 @@ sap.ui.define([
     "use strict";
 
     /**
-     * Constructor for ContextBinding
+     * @summary Constructor for ContextBinding
      *
      * @class
-     * Provides a specialized context binding that can be used to bind to a single
-     * document in a Meteor Mongo Collection.  Each instance of this class
-     * observes changes on a query handle to provide reactive updates via firing
+     * @description Provides a specialized context binding that can be used to
+     * bind to a single document in a Meteor Mongo Collection.  It is this type
+     * of ContextBinding that is used when, for example, an object header is
+     * bound to a path like "/Orders(<_id>)".
+     *
+     * Each instance of this class observes changes on a query handle to provide reactive updates via firing
      * events.
      *
      * @param {meteor-ui5.model.mongo.Model} oModel
@@ -43,13 +46,75 @@ sap.ui.define([
       }
     });
 
+    /**
+     * @summary Clean-up no longer needed resources when this ContextBinding is
+     * destroyed.
+     *
+     * @description Stop observing changes in the existing query or it will run
+     * forever.
+     * @public
+     */
     cContextBinding.prototype.destroy = function() {
-      // Stop observing changes in any existing query.  Will run forever otherwise.
       if (this._oQueryHandle) {
         this._oQueryHandle.stop();
       }
     };
 
+
+    /**
+     * @summary Check for changes or optionally force refresh
+     *
+     * @description Code copied from sap.ui.model.ClientContextBinding
+     * @public
+     */
+    cContextBinding.prototype.refresh = function(bForceUpdate) {
+      var that = this;
+      //recreate Context: force update
+      this.oModel.createBindingContext(this.sPath, this.oContext, this.mParameters, function(oContext) {
+        if (that.oElementContext === oContext && !bForceUpdate) {
+          that.oModel.checkUpdate(true, oContext);
+        } else {
+          that.oElementContext = oContext;
+          that._fireChange();
+        }
+      }, true);
+    };
+
+    /**
+     * @description Code copied from sap.ui.model.ClientContextBinding
+     * @public
+     */
+    cContextBinding.prototype.initialize = function() {
+      var that = this;
+      //recreate Context: force update
+      this.oModel.createBindingContext(this.sPath, this.oContext, this.mParameters, function(oContext) {
+        that.oElementContext = oContext;
+        that._fireChange();
+      }, true);
+    };
+
+    /**
+     * @description Code copied from sap.ui.model.ClientContextBinding
+     * @public
+     */
+    cContextBinding.prototype.setContext = function(oContext) {
+      var that = this;
+      if (this.oContext != oContext) {
+        this.oContext = oContext;
+        this.oModel.createBindingContext(this.sPath, this.oContext, this.mParameters, function(oContext) {
+          that.oElementContext = oContext;
+          that._fireChange();
+        });
+      }
+    };
+
+    /**
+     * @summary Execute Mongo query for current path and context and observe changes.
+     *
+     * @description This method runs the query for this ContextBinding and provides
+     * reactivity by observing changes in the query and firing events on change.
+     * @private
+     */
     cContextBinding.prototype._runQuery = function() {
       // Stop observing changes in any existing query.  Will run forever otherwise.
       if (this._oQueryHandle) {
@@ -75,49 +140,6 @@ sap.ui.define([
         }
       });
     }
-
-
-    /**
-     * @see sap.ui.model.ContextBinding.prototype.refresh
-     */
-    cContextBinding.prototype.refresh = function(bForceUpdate) {
-      var that = this;
-      //recreate Context: force update
-      this.oModel.createBindingContext(this.sPath, this.oContext, this.mParameters, function(oContext) {
-        if (that.oElementContext === oContext && !bForceUpdate) {
-          that.oModel.checkUpdate(true, oContext);
-        } else {
-          that.oElementContext = oContext;
-          that._fireChange();
-        }
-      }, true);
-    };
-
-    /**
-     * @see sap.ui.model.ContextBinding.prototype.refresh
-     */
-    cContextBinding.prototype.initialize = function() {
-      var that = this;
-      //recreate Context: force update
-      this.oModel.createBindingContext(this.sPath, this.oContext, this.mParameters, function(oContext) {
-        that.oElementContext = oContext;
-        that._fireChange();
-      }, true);
-    };
-
-    /**
-     * @see sap.ui.model.ContextBinding.prototype.setContext
-     */
-    cContextBinding.prototype.setContext = function(oContext) {
-      var that = this;
-      if (this.oContext != oContext) {
-        this.oContext = oContext;
-        this.oModel.createBindingContext(this.sPath, this.oContext, this.mParameters, function(oContext) {
-          that.oElementContext = oContext;
-          that._fireChange();
-        });
-      }
-    };
 
     return cContextBinding;
 
